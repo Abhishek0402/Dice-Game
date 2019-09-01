852483607493465ce82b94e5b2703a56132c61e4
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2019-09-01 19:14:10 +0530

    images of dice changes as per dice value

--- app.js
+++ app.js
@@ -9,14 +9,56 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0; // 0 for player1 and 1  for player 2
- 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //generate random number 
+    var dice = Math.floor(Math.random() * 6) + 1;
+   //display the result
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+    //update the round score if the rolled number was not 1 
+  if(dice !== 1)  {
+      //add score 
+      roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }
+  else {
+      //next player
+    activePlayer === 0 ?  activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+   
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+   
+    document.querySelector('.dice').style.display = 'none';
+
+}
+
+
+});
+
+
+ 
+
+
+
+
+
  
