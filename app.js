d10ba4df02985929223c088929395d852c93d016
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2019-09-02 07:46:41 +0530

    winner and new game feature added

--- app.js
+++ app.js
@@ -10,16 +10,10 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0; // 0 for player1 and 1  for player 2
 
-document.querySelector('.dice').style.display = 'none';
+initial();
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
+
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     //generate random number 
@@ -38,7 +32,38 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   }
   else {
-      //next player
+    nextPlayer();
+}
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function (){
+  
+    //add current score to parent or global score
+   scores[activePlayer] += roundScore;
+
+    //update the UI
+document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //CHECK IF player won the game
+
+     if(scores[activePlayer] >= 20){
+            document.querySelector('#name-' + activePlayer).textContent = 'WINNER!!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');     
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+     }
+            else {
+   //next player
+   nextPlayer();
+
+}
+ 
+
+});
+
+function nextPlayer() {
+
+    // to switch to the next player
     activePlayer === 0 ?  activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
     document.getElementById('current-0').textContent = '0';
@@ -48,17 +73,29 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('.player-1-panel').classList.toggle('active');
    
     document.querySelector('.dice').style.display = 'none';
-
 }
 
 
-});
+document.querySelector('.btn-new').addEventListener('click', initial);
+
+function initial() {
+    scores = [0,0];
+roundScore = 0;
+activePlayer = 0; // 0 for player1 and 1  for player 2
 
 
- 
+document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');   
+document.querySelector('.player-1-panel').classList.remove('winner');   
+document.querySelector('.player-0-panel').classList.remove('active');
 
-
-
-
- 
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+}
